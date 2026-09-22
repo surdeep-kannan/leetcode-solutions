d72e5f0b7a5370adba5064fd24dcc8f8b9f9b28f
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 08:42:25 +0530

    solve: #1790046736935 While Loop [Medium]

---
+++ GeeksForGeeks/while_loop.cpp
@@ -0,0 +1,18 @@
+// While Loop [Medium]
+// https://www.geeksforgeeks.org/problems/while-loop/
+
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        // code here
+        while(x>=0){
+            System.out.print(x+" ");
+            x--;
+        }
+    }
+}
