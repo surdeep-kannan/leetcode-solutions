476119b165d3b7de5663c4a0905d282f4df1373e
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 08:46:16 +0530

    solve: #1790046974391 Multiplication Table [Medium]

---
+++ GeeksForGeeks/multiplication_table.cpp
@@ -0,0 +1,16 @@
+// Multiplication Table [Medium]
+// https://www.geeksforgeeks.org/problems/multiplication-table/
+
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=10;i++){
+            System.out.print(n*i+" ");
+        }
+    }
+}
