f81dfef13859db67215a892fee9c7f754c2a5216
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 10:04:41 +0530

    solve: #1790051679222 Floyds Triangle [Medium]

--- GeeksForGeeks/floyds_triangle.cpp
+++ GeeksForGeeks/floyds_triangle.cpp
@@ -1,21 +1,5 @@
 // Floyds Triangle [Medium]
 // https://www.geeksforgeeks.org/problems/floyds-triangle/
 
-import java.util.Scanner;
+                System.out.print(c+" );
 
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // code here
-        int c=1;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                System.out.print(c+" ");
-                c++;
-            }
-            System.out.println("");
-        }
-    }
-}
