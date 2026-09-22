a63198477b3b4ea693884c10799e880f267f206b
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 09:17:20 +0530

    solve: #1790048836552 Reverse Coding [Medium]

---
+++ GeeksForGeeks/reverse_coding.cpp
@@ -0,0 +1,21 @@
+// Reverse Coding [Medium]
+// https://www.geeksforgeeks.org/problems/reverse-coding/
+
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result=sum(n);
+        // code here
+        System.out.print(result);
+        
+    }
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+}
