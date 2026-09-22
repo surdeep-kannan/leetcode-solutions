6a3d8ffb72193ac39eb7a0b82c14ccb9890aff30
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 08:52:24 +0530

    solve: #1790047340729 The Else If Statement [Medium]

--- GeeksForGeeks/the_else_if_statement.cpp
+++ GeeksForGeeks/the_else_if_statement.cpp
@@ -1,17 +1,5 @@
 // The Else If Statement [Medium]
 // https://www.geeksforgeeks.org/problems/the-else-if-statement/
 
-class Solution {
-    public static void utility(int number) {
-        // Write the if, else if, else statements below
-        if(number>100){
-            System.out.println("Big");
-        }
-        else if(number<10){
-            System.out.println("Small");
-        }
-        else{
             System.out.println("Number");
-        }
-    }
-}
+
