2fb8608a5c546249c44e0f8583c16e92d5e414fe
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-22 08:57:18 +0530

    solve: #1790047635573 Odd Or Even [Medium]

---
+++ GeeksForGeeks/odd_or_even.cpp
@@ -0,0 +1,14 @@
+// Odd Or Even [Medium]
+// https://www.geeksforgeeks.org/problems/odd-or-even/
+
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n%2!=0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
