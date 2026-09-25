c8697cd8e843e1556088d559a3d721475544c54a
surdeep-kannan <surdeepkannan2124@gmail.com>
2026-09-25 21:40:55 +0530

    solve: #135 Candy [Hard]

---
+++ LeetCode/candy.java
@@ -0,0 +1,25 @@
+// Candy [Hard]
+// https://leetcode.com/problems/candy/
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        int total = candies[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+            total += candies[i];
+        }
+        return total;
+    }
+}
